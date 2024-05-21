20e96dd6efcf9246434d98f27289f96ca31ee616
hyejin <hyejda00@naver.com>
2024-05-21 16:13:52 +0900

    제출 test

---
+++ src/main/java/com/sparta/spartaexam/SpartaExamApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartaexam/SpartaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
